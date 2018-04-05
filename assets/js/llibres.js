397a40f65eb6ddebed116a9e13eb92fa67b51339
Roger Forner Fabre <hello@rogerforner.com>
2018-04-05 19:51:58 +0200

    Mostrar Resum Relats Ok
    Hi ha un petit problema amb els salts de línia.

---
+++ assets/js/llibres.js
@@ -0,0 +1,74 @@
+/*******************************************************************************
+# Relat Curt
+# Relat Llarg
+*******************************************************************************/
+
+/*
+# Relat Curt
+*******************************************************************************/
+/*
+Gulliver's Travels
+------------------------------------------------------------------------------*/
+var xhttpGulliver = new XMLHttpRequest();
+xhttpGulliver.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    document.getElementById("resumGulliver").innerHTML = this.responseText;
+  }
+};
+xhttpGulliver.open("GET", "relats/f-gullivers-travels-into-several-remote-regions-of-the-world-short.txt", true);
+xhttpGulliver.send();
+
+/*
+Time and the Gods
+------------------------------------------------------------------------------*/
+var xhttpTime = new XMLHttpRequest();
+xhttpTime.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    document.getElementById("resumTime").innerHTML = this.responseText;
+  }
+};
+xhttpTime.open("GET", "relats/f-time-and-the-gods-short.txt", true);
+xhttpTime.send();
+
+/*
+A Princess of Mars
+------------------------------------------------------------------------------*/
+var xhttpPrincess = new XMLHttpRequest();
+xhttpPrincess.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    document.getElementById("resumPrincess").innerHTML = this.responseText;
+  }
+};
+xhttpPrincess.open("GET", "relats/sf-a-princess-of-mars-short.txt", true);
+xhttpPrincess.send();
+
+/*
+The War of the Worlds
+------------------------------------------------------------------------------*/
+var xhttpWorlds = new XMLHttpRequest();
+xhttpWorlds.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    document.getElementById("resumWorlds").innerHTML = this.responseText;
+  }
+};
+xhttpWorlds.open("GET", "relats/sf-the-war-of-the-worlds-short.txt", true);
+xhttpWorlds.send();
+
+/*
+# Relat Llarg
+*******************************************************************************/
+/*
+Gulliver's Travels
+------------------------------------------------------------------------------*/
+
+/*
+Time and the Gods
+------------------------------------------------------------------------------*/
+
+/*
+A Princess of Mars
+------------------------------------------------------------------------------*/
+
+/*
+The War of the Worlds
+------------------------------------------------------------------------------*/
